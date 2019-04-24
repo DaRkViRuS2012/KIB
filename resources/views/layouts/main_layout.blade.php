a2a775cd4ede9e165d30f97ada10bff69af7d8ec
Hamzayaghi5 <hamzayaghi2@gmail.com>
2019-04-25 02:54:38 +0300

    create UserController

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -26,7 +26,7 @@
    <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
   <div class="container">
     <a class="navbar-brand" href="#">
-        <img src="{{ asset('main_site/img/cover.jpg') }}" class="rounded-circle img-responsive" style="width:60px;height:60px">
+        <img src="{{ asset('main_site/img/logo.gif') }}" class="rounded-circle img-responsive" style="width:60px;height:60px">
         </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -77,7 +77,7 @@
           </div>
 
           <div class="row" style="margin-bottom:5%">
-            <img src="{{ asset('main_site/img/cover.jpg') }}" class="rounded-circle img-responsive" style="width:60px;height:60px">
+            <img src="{{ asset('main_site/img/logo.gif') }}" class="rounded-circle img-responsive" style="width:60px;height:60px">
           </div>
           
           <div class="row" style="margin-bottom:5%">
